8c9c4faaaff615a9b16671de5637b44260b0c5fc
610501 <restynan@gmail.com>
2019-11-27 15:25:00 -0600

    toast added

--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,16 +20,19 @@ class MainActivity : AppCompatActivity() {
         var num= ArrayList<String>(arrayListOf
             ("Hamburger", "Pizza", "Mexican","American", "Chinese"))
 
-        button2.setOnClickListener{
+
+        AddFood.setOnClickListener{
+           var foodItem=food.text.toString()
+            num.add(foodItem)
+            food.text=null
+            Toast.makeText(this, "$foodItem  added successfully", Toast.LENGTH_LONG).show()
+
+        }
+        Decide.setOnClickListener{
             // Change the Text view
             var randomIndex = Random().nextInt(num.size)
             var randomName =num.get(randomIndex)
             msg.text= randomName;
-
-        }
-        button1.setOnClickListener{
-
-            num.add(food.text.toString())
 
         }
     }
